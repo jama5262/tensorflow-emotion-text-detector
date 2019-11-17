6fcd5b42b6bf95bd7153cf0e2dac643628028cd0
Jama Mohamed <jama3137@gmail.com>
2019-11-17 20:29:39 +0300

    Added the textarea and the button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button, Spinner } from "react-bootstrap";
+
+import { TextArea } from "../components/TextArea";
 
 export const Home = () => {
   return (
     <Container>
       <Row>
-        <Col style={{textAlign: "center", marginTop: "90px"}} md={{ span: 10, offset: 1 }}>
-          <h1 style={{padding: "0 200px"}} className="display-5">Tensorflow Emotion Text Detector</h1>
+        <Col style={{ textAlign: "center", marginTop: "90px" }} md={{ span: 10, offset: 1 }}>
+          <h1 style={{ margin: "0 200px" }} className="display-5">Tensorflow Emotion Text Detector</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col style={{ textAlign: "center", marginTop: "50px" }} md={{ span: 10, offset: 1 }}>
+          <TextArea />
+        </Col>
+      </Row>
+      <Row>
+        <Col style={{ textAlign: "center", marginTop: "30px" }} md={{ span: 10, offset: 1 }}>
+          <Button variant="primary">
+            <Spinner
+              style={{margin: "0 10px"}}
+              animation="grow"
+              size="sm"
+              role="status"
+              aria-hidden="true"
+            />
+            Primary
+          </Button>
         </Col>
       </Row>
     </Container>
