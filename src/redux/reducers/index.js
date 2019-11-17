aa5e25efd41d00c17ef763e3865e42626f62861f
Jama Mohamed <jama3137@gmail.com>
2019-11-17 22:55:33 +0300

    Added the sub main emotion reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 
 import { query } from "./queryReducer"
 import { mainEmotion } from "./mainEmotionReducer"
+import { subEmotion } from "./subEmotionReducer"
 
 export const reducer = combineReducers({
   query,
-  mainEmotion
+  mainEmotion,
+  subEmotion
 });
---
+++ src/redux/reducers/subEmotionReducer.js
@@ -0,0 +1,16 @@
+import * as types from "../../utils/constants/actionContants"
+
+const initialState = {
+  data: []
+}
+
+export const subEmotion = (state = initialState, action) => {
+  switch (action.type) {
+    case types.ADD_SUB_EMOTION:
+      state = { ...state, data: action.subEmotion }
+      break;
+    default:
+      break;
+  }
+  return state
+}
