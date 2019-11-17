5f2d05e223be845f05c7be4ae078c0160906ec0a
Jama Mohamed <jama3137@gmail.com>
2019-11-17 14:52:00 +0300

    Worked with docker tf model and returned results

--- app/repository/repository.py
+++ app/repository/repository.py
@@ -1,5 +1,7 @@
 from app.utils.validate import isQueryValid, isQueryEmpty
 from app.utils.return_messages import success, error
+import requests
+import json
 
 
 def predict(request):
@@ -8,5 +10,27 @@ def predict(request):
     elif not isQueryEmpty(request):
         return error(422, "Looks like you didn't pass anything")
 
+    query = request.args.get("query")
+
+    tfModelRequestBody = {
+        "signature_name": "classification",
+        "examples": [{
+            "text": query
+        }]
+    }
+
+    response = requests.post(
+        "http://localhost:8501/v1/models/emo-model:classify", json=tfModelRequestBody)
+
+    emotions = ['sadness', 'joy', 'love', 'anger', 'fear', 'surprise']
+
+    predictedEmotions = response.json()["results"][0]
+
+    results = {}
+
+    for index, prediction in enumerate(response.json()["results"][0]):
+        results.update({emotions[index]: prediction[1]})
     
-    return success(200, "This all ok")
+    results = sorted(results.items(), key=lambda x: x[1], reverse=True)[:3]
+
+    return success(200, results)
