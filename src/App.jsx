60644f2614169909c3221d7061e2fb55ffd1082b
Jama Mohamed <jama3137@gmail.com>
2019-11-17 17:18:29 +0300

    [Started] Add the home file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from "react-bootstrap";
 
 export const App = () => {
   return (
     <div>
       <h1>Tensorflow React UI</h1>
+      <Button variant="dark">Primary</Button>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>This is home</h1>
+    </div>
+  )
+}
