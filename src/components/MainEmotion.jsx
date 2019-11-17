615f582bcfe31fd47ed998bbc007695c61c88b54
Jama Mohamed <jama3137@gmail.com>
2019-11-17 22:04:50 +0300

    Added sub emotion component

--- src/components/MainEmotion.jsx
+++ src/components/MainEmotion.jsx
@@ -8,7 +8,7 @@ export const MainEmotion = () => {
         <Image style={{ width: "500px" }} src="https://media.giphy.com/media/3o7bu3hn2X5NN9tnCE/giphy.gif" rounded />
       </Col>
       <Col xs={12}>
-        <span>Emotion <span style={{ fontSize: "30px" }} role="img" aria-label="emoji">😄</span></span>
+        <span>Sadness <span style={{ fontSize: "30px" }} role="img" aria-label="emoji">😄</span></span>
       </Col>
       <Col xs={12} style={{ marginTop: "10px" }}>
         <span style={{ color: "#66BB6A" }} className="display-4">74%</span>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,14 @@ import { Container, Row, Col, Button, Spinner } from "react-bootstrap";
 
 import { TextArea } from "../components/TextArea";
 import { MainEmotion } from "../components/MainEmotion";
+import { SubEmotion } from "../components/SubEmotion";
 
 export const Home = () => {
   return (
     <Container>
-      <Row>
-        <Col style={{ textAlign: "center", marginTop: "90px" }} md={{ span: 10, offset: 1 }}>
-          <h1 style={{ margin: "0 200px" }} className="display-5">Tensorflow Test Text Detector</h1>
+      <Row className="justify-content-sm-center">
+        <Col style={{ marginTop: "90px" }} md={{ span: 7 }}>
+          <h1 style={{ textAlign: "center" }} className="display-5">Tensorflow Test Text Detector</h1>
         </Col>
       </Row>
       <Row>
@@ -34,8 +35,18 @@ export const Home = () => {
       <Row>
         <Col style={{ textAlign: "center", marginTop: "50px" }} md={{ span: 10, offset: 1 }}>
           <MainEmotion/>
+          <hr/>
         </Col>
       </Row>
+      <Row className="justify-content-sm-center">
+        <Col style={{ textAlign: "center", marginTop: "50px" }} xs={{ span: 3 }}>
+          <SubEmotion/>
+        </Col>
+        <Col style={{ textAlign: "center", marginTop: "50px" }} xs={{ span: 3 }}>
+          <SubEmotion/>
+        </Col>
+      </Row>
+      <div style={{marginBottom: "100px"}}></div>
     </Container>
   )
 }
